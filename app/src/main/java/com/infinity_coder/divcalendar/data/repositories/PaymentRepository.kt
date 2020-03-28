4f80f098364192396b4d0e39967be2bf9ac24f64
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-03-28 14:25:41 +0300

    Создал репозиторий (PaymentsRepository) и функцию loadAllPayments, которая возвращает Mock данные

---
+++ app/src/main/java/com/infinity_coder/divcalendar/data/repositories/PaymentRepository.kt
@@ -0,0 +1,27 @@
+package com.infinity_coder.divcalendar.data.repositories
+
+import com.infinity_coder.divcalendar.data.network.model.PaymentNetworkModel
+
+object PaymentRepository {
+
+    suspend fun loadAllPayments():List<PaymentNetworkModel>{
+        val mockData = mutableListOf<PaymentNetworkModel>()
+
+        for(i in 0 until 12){
+            for(j in 0 until 25){
+                mockData.add(
+                    PaymentNetworkModel(
+                        "Яндекс",
+                        "https://s0.rbk.ru/emitent_pics/resized/80x80_crop/images/20/74/3c2996c034c70685ec736cd563e55dd6.png",
+                        15,
+                        174.05,
+                        "2020-${if(i.toString().length==1) "0$i" else i.toString()}-${if(j.toString().length==1) "0$j" else j.toString()}"
+                    )
+                )
+            }
+        }
+
+        return mockData
+    }
+
+}
