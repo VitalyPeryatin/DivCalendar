bb7116640d4d268b8f562bf4fdf4cdb3e788d1f2
VitalyPeryatin <peryatin.vitalik37@gmail.com>
2020-04-03 19:18:36 +0300

    Add LeakCanary

--- app/src/main/java/com/infinity_coder/divcalendar/presentation/AppActivityLifecycleCallbacks.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/presentation/AppActivityLifecycleCallbacks.kt
@@ -8,25 +8,6 @@ import android.os.Bundle
 import com.infinity_coder.divcalendar.BuildConfig
 
 class AppActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
-    override fun onActivityPaused(activity: Activity) {
-
-    }
-
-    override fun onActivityStarted(activity: Activity) {
-
-    }
-
-    override fun onActivityDestroyed(activity: Activity) {
-
-    }
-
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-
-    }
-
-    override fun onActivityStopped(activity: Activity) {
-
-    }
 
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
@@ -35,7 +16,21 @@ class AppActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
         }
     }
 
-    override fun onActivityResumed(activity: Activity) {
+    override fun onActivityPaused(activity: Activity) {
+    }
 
+    override fun onActivityStarted(activity: Activity) {
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+    }
+
+    override fun onActivityResumed(activity: Activity) {
     }
 }
