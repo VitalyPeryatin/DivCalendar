2c1d2ef8b972b92ba0b03de4d521fc928c904091
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-04-14 23:13:17 +0300

    Application availability check has been added

---
+++ app/src/main/java/com/infinity_coder/divcalendar/presentation/splash/SplashScreenActivity.kt
@@ -0,0 +1,39 @@
+package com.infinity_coder.divcalendar.presentation.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
+import com.infinity_coder.divcalendar.presentation.main.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getAccess()
+    }
+
+    private fun getAccess(){
+        FirebaseFirestore.getInstance()
+            .collection(COLLECTION_NAME)
+            .get()
+            .addOnCompleteListener {
+                if(!it.isSuccessful || it.result == null) return@addOnCompleteListener
+
+                if(it.result!!.isHaveAccess()){
+                    startActivity(Intent(this, MainActivity::class.java))
+                }
+            }
+    }
+
+    private fun QuerySnapshot.isHaveAccess():Boolean{
+        return (documents.find { it.id == DOC_NAME }?.data?.get(FIELD_NAME) as? Boolean) ?: false
+    }
+
+    companion object{
+        private const val COLLECTION_NAME = "test_application"
+        private const val DOC_NAME = "access"
+        private const val FIELD_NAME = "haveAccess"
+    }
+}
