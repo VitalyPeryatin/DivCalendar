1c685009a54fafcb58b5ddbe6754642ce94da016
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-04-18 00:14:34 +0300

    Fixed comments

--- app/src/main/java/com/infinity_coder/divcalendar/presentation/_common/SecurityCurrencyDelegate.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/presentation/_common/SecurityCurrencyDelegate.kt
@@ -3,11 +3,22 @@ package com.infinity_coder.divcalendar.presentation._common
 import android.content.Context
 import com.infinity_coder.divcalendar.R
 import com.infinity_coder.divcalendar.data.repositories.RateRepository
-import java.util.*
+import java.text.DecimalFormat
 
 object SecurityCurrencyDelegate {
 
-    fun getCurrencyBadge(context: Context, currency: String): String {
+    fun getValueWithCurrency(context: Context, value: Float, currency: String, accuracy: Int = 2): String {
+        val formatter = DecimalFormat("0.${"#".repeat(accuracy)}")
+        val valueStr = formatter.format(value)
+        val currencyBadge = getCurrencyBadge(context, currency)
+        return "$valueStr $currencyBadge"
+    }
+
+    fun getValueWithCurrency(context: Context, value: String, currency: String, accuracy: Int = 2): String {
+        return getValueWithCurrency(context, value.toFloat(), currency, accuracy)
+    }
+
+    private fun getCurrencyBadge(context: Context, currency: String): String {
         return when (currency) {
             RateRepository.RUB_RATE -> context.resources.getString(R.string.currency_rub_name)
             RateRepository.USD_RATE -> context.resources.getString(R.string.currency_usd_name)
@@ -15,39 +26,4 @@ object SecurityCurrencyDelegate {
         }
     }
 
-    fun getValueWithCurrency(context: Context, value: Float, currency: String, accuracy: Int = 2): String {
-        val valueStr = String.format(Locale.getDefault(), "%.${accuracy}f", value)
-        val newValueStr = deleteLastZeros(valueStr)
-        val currencyBadge = getCurrencyBadge(context, currency)
-        return "$newValueStr $currencyBadge"
-    }
-
-    fun getValueWithCurrency(context: Context, value: String, currency: String, accuracy: Int = 2): String {
-        return getValueWithCurrency(context, value.toFloat(), currency, accuracy)
-    }
-
-    private fun deleteLastZeros(valueStr: String): String {
-        val wholePart: String
-        val fractionPart: String
-        valueStr.split(",").run {
-            wholePart = this[0]
-            fractionPart = this[1]
-        }
-
-        var newFractionPart = ""
-
-        for (i in (fractionPart.length - 1) downTo 0) {
-            val number = fractionPart[i]
-
-            if (number != '0' || newFractionPart.isNotEmpty()) {
-                newFractionPart = number + newFractionPart
-            }
-        }
-
-        return if (newFractionPart.isNotEmpty()) {
-            "$wholePart,$newFractionPart"
-        } else {
-            wholePart
-        }
-    }
 }
