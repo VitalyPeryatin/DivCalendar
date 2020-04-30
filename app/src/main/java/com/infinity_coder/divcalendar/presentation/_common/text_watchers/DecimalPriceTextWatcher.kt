b6127e6ce56c427038b177f82dba4ae2326f486c
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-04-30 23:13:40 +0300

    correct errors

--- app/src/main/java/com/infinity_coder/divcalendar/presentation/_common/text_watchers/DecimalPriceTextWatcher.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/presentation/_common/text_watchers/DecimalPriceTextWatcher.kt
@@ -3,6 +3,7 @@ package com.infinity_coder.divcalendar.presentation._common.text_watchers
 import android.annotation.SuppressLint
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
 import java.text.DecimalFormat
 
@@ -16,6 +17,7 @@ class DecimalPriceTextWatcher(
     private var enteredNumericBeforeChange: String = ""
     private var selectionStartBeforeUserChange: Int = 0
     private var isAddition = false
+    private var currentSeparator = LOCAL_SEPARATOR
 
     override fun afterTextChanged(s: Editable?) {
     }
@@ -28,7 +30,10 @@ class DecimalPriceTextWatcher(
 
     @SuppressLint("SetTextI18n")
     override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        val enteredNumeric = s.toString().replace(decimalFormat.decimalFormatSymbols.groupingSeparator.toString(), "")
+        defineCurrentSeparator(s.toString())
+        val enteredNumeric = s.toString()
+            .replace(decimalFormat.decimalFormatSymbols.groupingSeparator.toString(), "")
+            .replace(currentSeparator.toString(), LOCAL_SEPARATOR.toString())
 
         if (checkIfFirstCharacterSeparatorWhenAdding(enteredNumeric) ||
             checkIfCountOfNumberInIntegerPartGreaterPossible(enteredNumeric) ||
@@ -46,11 +51,11 @@ class DecimalPriceTextWatcher(
     }
 
     private fun checkIfFirstCharacterSeparatorWhenAdding(enteredNumeric: String): Boolean {
-        return enteredNumeric.isNotEmpty() && enteredNumeric.first() == EDIT_TEXT_SEPARATOR && isAddition
+        return enteredNumeric.isNotEmpty() && enteredNumeric.first() == LOCAL_SEPARATOR && isAddition
     }
 
     private fun checkIfFirstCharacterSeparatorWhenRemoved(enteredNumeric: String): Boolean {
-        return enteredNumeric.isNotEmpty() && enteredNumeric.first() == EDIT_TEXT_SEPARATOR && !isAddition
+        return enteredNumeric.isNotEmpty() && enteredNumeric.first() == LOCAL_SEPARATOR && !isAddition
     }
 
     private fun checkIfCountOfNumberInIntegerPartGreaterPossible(enteredNumeric: String): Boolean {
@@ -78,7 +83,7 @@ class DecimalPriceTextWatcher(
 
         val formattedEnteredNumeric = decimalFormat.format(numericParts.integerPart.toDouble()).run {
             if (numericParts.fractionalPart != null) {
-                return@run "$this.${numericParts.fractionalPart}"
+                return@run "$this$currentSeparator${numericParts.fractionalPart}"
             }
             return@run this
         }
@@ -97,8 +102,10 @@ class DecimalPriceTextWatcher(
                         editText.setSelection(selectionStartBeforeChange)
                 }
                 else -> {
-                    if (enteredNumericBeforeChange.length == formattedEnteredNumeric.length)
+                    if (enteredNumericBeforeChange.length == formattedEnteredNumeric.length){
                         editText.setSelection(selectionStartBeforeUserChange)
+                        Log.d("LogEdit","fuck")
+                    }
                     else
                         editText.setSelection(selectionStartBeforeChange)
                 }
@@ -122,10 +129,10 @@ class DecimalPriceTextWatcher(
         val integerPart: String
         var fractionalPart: String? = null
 
-        enteredNumeric.split(EDIT_TEXT_SEPARATOR).run {
-            integerPart = this[0]
-            if (this.size > 1)
-                fractionalPart = this[1]
+        enteredNumeric.split(LOCAL_SEPARATOR).let {
+            integerPart = it[0]
+            if (it.size > 1)
+                fractionalPart = it[1]
         }
 
         return NumericParts(integerPart, fractionalPart)
@@ -137,8 +144,17 @@ class DecimalPriceTextWatcher(
         editText.addTextChangedListener(this)
     }
 
+    private fun defineCurrentSeparator(currentEnteredNumeric:String){
+        for(separator in POSSIBLE_SEPARATORS){
+            if(currentEnteredNumeric.contains(separator)){
+                currentSeparator = separator
+            }
+        }
+    }
+
     companion object {
-        private const val EDIT_TEXT_SEPARATOR = '.'
+        private const val LOCAL_SEPARATOR = '.'
+        private val POSSIBLE_SEPARATORS = listOf('.',',')
     }
 
     data class NumericParts(
