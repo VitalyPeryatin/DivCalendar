487e34c2f7c44988271227a3487334524c7ea6ee
VitalyPeryatin <peryatin.vitalik37@gmail.com>
2020-05-03 10:57:11 +0300

    Fix ProGuard for Apache POI

--- app/src/main/java/com/infinity_coder/divcalendar/presentation/App.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/presentation/App.kt
@@ -17,6 +17,10 @@ class App : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLInputFactory", "com.fasterxml.aalto.stax.InputFactoryImpl")
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLOutputFactory", "com.fasterxml.aalto.stax.OutputFactoryImpl")
+        System.setProperty("org.apache.poi.javax.xml.stream.XMLEventFactory", "com.fasterxml.aalto.stax.EventFactoryImpl")
+
         instance = this
         context = applicationContext
         registerActivityLifecycleCallbacks(AppActivityLifecycleCallbacks())
