528e2d74a773cba70e690cf2e2e555b2a8c60edd
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-05-29 11:55:47 +0300

    removed news from the project

--- app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
@@ -1,16 +1,21 @@
 package com.infinity_coder.divcalendar.data.db
 
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.infinity_coder.divcalendar.presentation.App
 
 object DivCalendarDatabase {
+
     val roomDatabase: AppDatabase by lazy {
-        Room.databaseBuilder(
-            App.instance.applicationContext,
-            AppDatabase::class.java,
-            "div-calendar-database"
-        )
-            .fallbackToDestructiveMigration()
+        Room.databaseBuilder(App.instance.applicationContext, AppDatabase::class.java, "div-calendar-database")
+            .addMigrations(MIGRATION_1_2)
             .build()
+    }
+
+    private val MIGRATION_1_2 = object : Migration(1,2){
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("DROP TABLE Post")
+        }
     }
 }
