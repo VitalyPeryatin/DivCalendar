2c64a1089ead5210fe9a92f022f2436cdf7f57f5
snakelord757 <blackkiller1527@gmail.com>
2020-06-20 17:41:32 +0300

    Correct migration

--- app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
@@ -24,8 +24,17 @@ object DivCalendarDatabase {
     private val MIGRATION_2_3 = object : Migration(2, 3) {
         override fun migrate(database: SupportSQLiteDatabase) {
             database.apply {
-                execSQL("ALTER TABLE ${PaymentDbModel.TABLE_NAME} MODIFY ${PaymentDbModel.COLUMN_DIVIDENDS} TEXT, ${PaymentDbModel.COLUMN_COUNT} TEXT")
-                execSQL("ALTER TABLE ${SecurityDbModel.TABLE_NAME} MODIFY ${SecurityDbModel.COLUMN_COUNT} TEXT, ${SecurityDbModel.COLUMN_TOTAL_PRICE} TEXT")
+                execSQL("CREATE TABLE NewPayment(dividends TEXT NOT NULL, date TEXT NOT NULL, forecast INTEGER NOT NULL, isin TEXT NOT NULL, portfolio_id INTEGER NOT NULL, count TEXT NOT NULL, PRIMARY KEY(date, isin, portfolio_id), FOREIGN KEY(isin, portfolio_id) REFERENCES Security(isin, portfolio_id) ON DELETE CASCADE)")
+                execSQL("INSERT INTO NewPayment(dividends, date, forecast, isin, portfolio_id, count) SELECT dividends, date, forecast, isin, portfolio_id, count FROM Payment")
+                execSQL("DROP TABLE Payment")
+                execSQL("CREATE INDEX security_index ON NewPayment(isin, portfolio_id)")
+                execSQL("ALTER TABLE NewPayment RENAME TO Payment")
+
+                execSQL("CREATE TABLE NewSecurity (isin TEXT NOT NULL, ticker TEXT NOT NULL, name TEXT NOT NULL, type TEXT NOT NULL, logo TEXT NOT NULL, year_yield REAL NOT NULL, exchange TEXT, currency TEXT NOT NULL, count TEXT NOT NULL, total_price TEXT NOT NULL, portfolio_id INTEGER NOT NULL, color INTEGER NOT NULL, PRIMARY KEY(isin, portfolio_id), FOREIGN KEY(portfolio_id) REFERENCES Portfolio(id) ON DELETE CASCADE)")
+                execSQL("INSERT INTO NewSecurity(isin, ticker, name, type, logo, year_yield, exchange, currency, count, total_price, portfolio_id, color) SELECT isin, ticker, name, type, logo, year_yield, exchange, currency, count, total_price, portfolio_id, color FROM Security")
+                execSQL("DROP TABLE Security")
+                execSQL("CREATE INDEX portfolio_id_index ON NewSecurity(portfolio_id)")
+                execSQL("ALTER TABLE NewSecurity RENAME TO Security")
             }
         }
     }
