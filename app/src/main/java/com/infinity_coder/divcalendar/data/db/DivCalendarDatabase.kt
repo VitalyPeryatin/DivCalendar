4228734a453964c4e1dd102d05afbaa0cc1a8504
eduard_abdulmanov@mail.ru <eduard_abdulmanov@mail.ru>
2020-06-22 13:17:41 +0300

    made a Security table migration

--- app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
+++ app/src/main/java/com/infinity_coder/divcalendar/data/db/DivCalendarDatabase.kt
@@ -3,6 +3,7 @@ package com.infinity_coder.divcalendar.data.db
 import androidx.room.Room
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.infinity_coder.divcalendar.data.db.model.PaymentDbModel
 import com.infinity_coder.divcalendar.data.db.model.PortfolioDbModel
 import com.infinity_coder.divcalendar.data.db.model.SecurityDbModel
 import com.infinity_coder.divcalendar.presentation.App
@@ -24,10 +25,12 @@ object DivCalendarDatabase {
     private val MIGRATION_2_3 = object : Migration(2, 3) {
         override fun migrate(database: SupportSQLiteDatabase) {
 
-            database.execSQL("CREATE TABLE `${SecurityDbModel.TABLE_NAME}_copy` (" +
+            database.execSQL("ALTER TABLE ${SecurityDbModel.TABLE_NAME} ADD COLUMN ${SecurityDbModel.COLUMN_MARKET} TEXT NOT NULL DEFAULT ''")
+
+            database.execSQL("CREATE TABLE ${SecurityDbModel.TABLE_NAME}_copy (" +
                     "${SecurityDbModel.COLUMN_ISIN} TEXT NOT NULL, " +
                     "${SecurityDbModel.COLUMN_TICKER} TEXT NOT NULL, " +
-                    "${SecurityDbModel.TABLE_NAME} TEXT NOT NULL, " +
+                    "${SecurityDbModel.COLUMN_NAME} TEXT NOT NULL, " +
                     "${SecurityDbModel.COLUMN_TYPE} TEXT NOT NULL, " +
                     "${SecurityDbModel.COLUMN_LOGO} TEXT NOT NULL, " +
                     "${SecurityDbModel.COLUMN_YEAR_YIELD} REAL NOT NULL, " +
@@ -37,15 +40,14 @@ object DivCalendarDatabase {
                     "${SecurityDbModel.COLUMN_TOTAL_PRICE} REAL NOT NULL, " +
                     "${SecurityDbModel.COLUMN_PORTFOLIO_ID} INTEGER NOT NULL, " +
                     "${SecurityDbModel.COLUMN_COLOR} INTEGER NOT NULL, " +
-                    "${SecurityDbModel.COLUMN_MARKET} TEXT NOT NULL," +
-                    "PRIMARY KEY (${SecurityDbModel.COLUMN_ISIN}, ${SecurityDbModel.COLUMN_PORTFOLIO_ID}, ${SecurityDbModel.COLUMN_EXCHANGE}, " +
-                    "FOREIGN KEY (${SecurityDbModel.COLUMN_PORTFOLIO_ID}) ${PortfolioDbModel.TABLE_NAME} (${PortfolioDbModel.COLUMN_ID} ON DELETE CASCADE)"
+                    "${SecurityDbModel.COLUMN_MARKET} TEXT NOT NULL, " +
+                    "PRIMARY KEY (${SecurityDbModel.COLUMN_ISIN}, ${SecurityDbModel.COLUMN_PORTFOLIO_ID}, ${SecurityDbModel.COLUMN_EXCHANGE}), " +
+                    "FOREIGN KEY (${SecurityDbModel.COLUMN_PORTFOLIO_ID}) REFERENCES ${PortfolioDbModel.TABLE_NAME} (${PortfolioDbModel.COLUMN_ID}) ON DELETE CASCADE)"
             )
-
-            database.execSQL("INSERT INTO `${SecurityDbModel.TABLE_NAME}_copy` (" +
+            database.execSQL("INSERT INTO ${SecurityDbModel.TABLE_NAME}_copy (" +
                     "${SecurityDbModel.COLUMN_ISIN}, " +
                     "${SecurityDbModel.COLUMN_TICKER}, " +
-                    "${SecurityDbModel.TABLE_NAME}, " +
+                    "${SecurityDbModel.COLUMN_NAME}, " +
                     "${SecurityDbModel.COLUMN_TYPE}, " +
                     "${SecurityDbModel.COLUMN_LOGO}, " +
                     "${SecurityDbModel.COLUMN_YEAR_YIELD}, " +
@@ -59,7 +61,7 @@ object DivCalendarDatabase {
                     "SELECT " +
                     "${SecurityDbModel.COLUMN_ISIN}, " +
                     "${SecurityDbModel.COLUMN_TICKER}, " +
-                    "${SecurityDbModel.TABLE_NAME}, " +
+                    "${SecurityDbModel.COLUMN_NAME}, " +
                     "${SecurityDbModel.COLUMN_TYPE}, " +
                     "${SecurityDbModel.COLUMN_LOGO}, " +
                     "${SecurityDbModel.COLUMN_YEAR_YIELD}, " +
@@ -71,12 +73,9 @@ object DivCalendarDatabase {
                     "${SecurityDbModel.COLUMN_COLOR}, " +
                     "${SecurityDbModel.COLUMN_MARKET} FROM ${SecurityDbModel.TABLE_NAME}"
             )
-
             database.execSQL("DROP TABLE ${SecurityDbModel.TABLE_NAME}")
-            database.execSQL("ALTER TABLE `${SecurityDbModel.TABLE_NAME}_copy` RENAME TO ${SecurityDbModel.TABLE_NAME}")
-
-            database.execSQL("INSERT INTO `${SecurityDbModel.TABLE_NAME}_copy` (`isin`, `ticker`, `name`) SELECT `isin`, `ticker`, `name` FROM ${SecurityDbModel.TABLE_NAME}")
-            database.execSQL("ALTER TABLE ${SecurityDbModel.TABLE_NAME} ADD COLUMN ${SecurityDbModel.COLUMN_MARKET} TEXT NOT NULL DEFAULT ''")
+            database.execSQL("ALTER TABLE ${SecurityDbModel.TABLE_NAME}_copy RENAME TO ${SecurityDbModel.TABLE_NAME}")
+            database.execSQL("CREATE INDEX IF NOT EXISTS ${SecurityDbModel.INDEX_PORTFOLIO_ID} ON ${SecurityDbModel.TABLE_NAME} (${PaymentDbModel.COLUMN_PORTFOLIO_ID})")
         }
     }
 }
